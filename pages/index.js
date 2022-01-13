594aeb152195ed6b6dc9361a2df86a5001be8900
Seak Digital <nsineke@gmail.com>
2022-01-13 12:51:18 +0200

    Add state to pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import styles from "../styles/Home.module.css";
 import List from "../components/list/list";
 import Title from "../components/title/title";
+import Button from "../components/button/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [bob, setBob] = useState("bar");
+
+  const handleOnClick = () => {
+    setBob("Fifa 2022");
+  };
+
   return (
     <div className={styles.container}>
       <Title customColor="blue">Awesome example</Title>
+      <p>{bob}</p>
       <List />
+      <Button
+        onClick={() => {
+          handleOnClick();
+        }}
+      >
+        Click Me
+      </Button>
     </div>
   );
 }
